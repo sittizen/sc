19b8b2a7636b757e1b58758fe3f18badec0abc03
Simone Cittadini <simone@sig-c.com>
2015-12-03 13:02:24 +0100

    abilitate connessioni persistenti al db.

--- sc/settings.py
+++ sc/settings.py
@@ -119,6 +119,8 @@ import dj_database_url
 DATABASES = {
     'default':  dj_database_url.config()
 }
+# Enable Persistent Connections
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
